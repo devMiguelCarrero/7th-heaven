5dee4a90e3890c979f682bfc0f3c94fea5a21651
devMiguelCarrero <developermiguelcarrero@gmail.com>
2022-09-02 11:42:56 -0500

    add Reactor 7 font

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import reactDom from "react-dom";
 import "./frameworks/psone.css/PSone.css";
+import "./assets/fonts/reactor7/reactor7.scss";
 import "./normalize.scss";
 import "./style.scss";
 import App from "./App";
