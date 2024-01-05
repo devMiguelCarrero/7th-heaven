c6557962f362c8d346ddf9e9f217b6777b90c951
Miguel.Carrero <Miguel.Carrero@gaincapital.com>
2024-01-05 13:35:22 -0500

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,20 @@
 import classes from './page.module.scss';
 import Image from 'next/image';
 
+import MainContainer from '@/components/Atoms/MainContainer/MainContainer';
+import SevenSection from '@/components/Atoms/Section/Section';
+import Materia from '@/components/Atoms/Materia/Materia';
+
 export default function Home() {
   return (
     <main className={classes.main}>
-      <div>
-        <p>7th Heaven</p>
-      </div>
+      <MainContainer>
+        <SevenSection variant="index-section">
+          <p>
+            7th Heaven <Materia variant="summon" name="knights of the round" />
+          </p>
+        </SevenSection>
+      </MainContainer>
     </main>
   );
 }
