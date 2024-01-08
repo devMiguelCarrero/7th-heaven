2f2553d5858812b3de1e702953df10a9a6a63872
Miguel.Carrero <Miguel.Carrero@gaincapital.com>
2024-01-07 20:28:52 -0500

    add transparent-area Atom

---
+++ src/components/Atoms/TransparentArea/TransparentArea.tsx
@@ -0,0 +1,30 @@
+import React, { FC, ReactNode } from 'react';
+import cx from 'classnames';
+
+import { classNames } from '@/shared/types/types';
+import classes from './transparent-area.module.scss';
+
+interface Props {
+  className?: classNames;
+  variant?: 'index-section';
+  children: ReactNode;
+}
+
+const TransparentArea: FC<Props> = ({
+  className,
+  variant,
+  children,
+  ...props
+}) => {
+  const classnames = cx(classes['transparent-area'], {
+    [className]: className,
+    [classes[`transparent-area--${variant}`]]: variant,
+  });
+
+  return (
+    <div {...props} className={classnames}>
+      {children}
+    </div>
+  );
+};
+export default TransparentArea;
