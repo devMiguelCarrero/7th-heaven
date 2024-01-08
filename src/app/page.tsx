a6252e2f5b1f4e3ad30203274d69c9ce36cf02f4
Miguel.Carrero <Miguel.Carrero@gaincapital.com>
2024-01-07 20:30:12 -0500

    update main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,8 @@ import Image from 'next/image';
 import MainContainer from '@/components/Atoms/MainContainer/MainContainer';
 import SevenSection from '@/components/Atoms/Section/Section';
 import Materia from '@/components/Atoms/Materia/Materia';
+import TransparentArea from '@/components/Atoms/TransparentArea/TransparentArea';
+import SevenMenu from '@/components/Molecules/SevenMenu/SevenMenu';
 
 export default function Home() {
   return (
@@ -14,6 +16,11 @@ export default function Home() {
             7th Heaven <Materia variant="summon" name="knights of the round" />
           </p>
         </SevenSection>
+        <TransparentArea variant="index-section">
+          <SevenSection variant="main-menu-section">
+            <SevenMenu />
+          </SevenSection>
+        </TransparentArea>
       </MainContainer>
     </main>
   );
