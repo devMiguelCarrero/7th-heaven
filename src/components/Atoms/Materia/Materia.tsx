b58bc3f499e7910acbe35a28e71e8e5b1fb85cef
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-10 14:15:00 -0500

    update Materia Atom
    add slug prop

--- src/components/Atoms/Materia/Materia.tsx
+++ src/components/Atoms/Materia/Materia.tsx
@@ -7,23 +7,27 @@ import classes from './materia.module.scss';
 import Link from 'next/link';
 
 interface Props {
-  variant?: 'magic' | 'support' | 'command' | 'independent' | 'summon';
+  variant?: string;
   className?: classNames;
   name: string;
+  slug: string | null;
 }
 
 const Materia: FC<Props> = ({
   variant = 'magic',
   className,
   name = 'bolt',
+  slug,
 }) => {
   const classNames = cx(classes['materia'], {
     [className]: className,
     [classes[`materia--${variant}`]]: variant,
   });
 
+  const materiaSlug = slug ? slug : '#';
+
   return (
-    <Link className={classNames} href="#">
+    <Link className={classNames} href={`/materia/${materiaSlug}`}>
       <span>{name}</span>
     </Link>
   );
