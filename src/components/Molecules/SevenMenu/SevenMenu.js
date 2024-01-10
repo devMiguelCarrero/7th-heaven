e7473ce4a41c835e4f1c3a89ff540be499a88e3c
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-10 14:10:05 -0500

    update Main Menu Molecule

--- src/components/Molecules/SevenMenu/SevenMenu.js
+++ src/components/Molecules/SevenMenu/SevenMenu.js
@@ -1,27 +1,33 @@
-import React from "react";
-import "./Menu.scss";
+import React from 'react';
+import Link from 'next/link';
+
+import './Menu.scss';
 
 const SevenMenu = (props) => {
   const data = [
-    { id: "1", title: "Item" },
-    { id: "2", title: "Magic" },
-    { id: "3", title: "Materia" },
-    { id: "4", title: "Equip" },
-    { id: "5", title: "Status" },
-    { id: "6", title: "Order" },
-    { id: "7", title: "Limit" },
-    { id: "8", title: "Config" },
-    { id: "space", title: "Item" },
-    { id: "9", title: "Save" },
+    { id: '1', title: 'Home', url: '/' },
+    { id: '2', title: 'About', url: '#' },
+    { id: '3', title: 'Materia', url: '/materia' },
+    { id: '4', title: 'Equip', url: '#' },
+    { id: '5', title: 'Status', url: '#' },
+    { id: '6', title: 'Order', url: '#' },
+    { id: '7', title: 'Limit', url: '#' },
+    { id: '8', title: 'Config', url: '#' },
+    { id: 'space', title: 'Item', url: '#' },
+    { id: '9', title: 'Save', url: '#' },
   ];
 
   return (
     <ul className="seven-vertical-menu">
       {data.map((item, index) => {
-        if (item.id === "space") {
+        if (item.id === 'space') {
           return <li key={`space-${index}`}></li>;
         }
-        return <li key={item.id}>{item.title}</li>;
+        return (
+          <li key={item.id}>
+            <Link href={item.url}>{item.title}</Link>
+          </li>
+        );
       })}
     </ul>
   );
