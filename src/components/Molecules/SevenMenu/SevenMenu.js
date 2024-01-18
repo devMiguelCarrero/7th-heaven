e1ee93715327c14d628b8521bc740af3e5da9e76
devMiguelCarrero <developermiguelcarrero@gmail.com>
2024-01-18 14:55:55 -0500

    update Molecule SevenMenu
    update menu links & styles

--- src/components/Molecules/SevenMenu/SevenMenu.js
+++ src/components/Molecules/SevenMenu/SevenMenu.js
@@ -2,19 +2,19 @@ import React from 'react';
 import Link from 'next/link';
 
 import './Menu.scss';
+import AtomLink from '@/components/Atoms/AtomLink';
 
 const SevenMenu = (props) => {
   const data = [
-    { id: '1', title: 'Home', url: '/' },
-    { id: '2', title: 'About', url: '#' },
-    { id: '3', title: 'Materia', url: '/materia' },
-    { id: '4', title: 'Equip', url: '#' },
-    { id: '5', title: 'Status', url: '#' },
-    { id: '6', title: 'Order', url: '#' },
-    { id: '7', title: 'Limit', url: '#' },
-    { id: '8', title: 'Config', url: '#' },
-    { id: 'space', title: 'Item', url: '#' },
-    { id: '9', title: 'Save', url: '#' },
+    { id: '1', title: 'Home', url: '/', disabled: false },
+    { id: '2', title: 'About', url: '#', disabled: true },
+    { id: '4', title: 'Characters', url: '#', disabled: true },
+    { id: '3', title: 'Materia', url: '/materia', disabled: false },
+    { id: '5', title: 'Enemy Skill', url: '#', disabled: true },
+    { id: '6', title: 'Chocobo', url: '#', disabled: true },
+    { id: '7', title: 'Limit', url: '#', disabled: true },
+    { id: '8', title: 'Credits', url: '#', disabled: true },
+    { id: '9', title: 'Enemies', url: '#', disabled: true },
   ];
 
   return (
@@ -25,7 +25,13 @@ const SevenMenu = (props) => {
         }
         return (
           <li key={item.id}>
-            <Link href={item.url}>{item.title}</Link>
+            <AtomLink
+              href={item.url}
+              title={item.title}
+              disabled={item.disabled}
+            >
+              {item.title}
+            </AtomLink>
           </li>
         );
       })}
